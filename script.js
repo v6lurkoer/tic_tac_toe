f17c23d428dbc8921c048b2f8e172c9f1de7f271
Christopher <chris.kuut@pm.me>
2026-09-13 00:55:55 +0300

    Add name button added, also form toggle works. Also added footer.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const game = (() => {
   ];
   let playerNames = [];
   let gameArray = [];
+  let secondPlayerTurn = false;
 
   const setValue = (index, value) => gameArray[index] = value;
   const getValue = (index) => { return gameArray[index]; }
@@ -19,6 +20,7 @@ const game = (() => {
   const newGame = () => {
     gameArray.length = 0;
     playerNames.length = 0;
+    secondPlayerTurn = false;
   };
   const checkForWin = () => {
     for (let i = 0; i < winConditions.length; i++) {
@@ -27,11 +29,24 @@ const game = (() => {
       const value3 = gameArray[winConditions[i][2]];
       if (value1 !== undefined && value2 !== undefined && value3 !== undefined) {
         if (value1 === value2 && value1 === value3) {
-          console.log("helloworld");
-          console.log(winConditions[i]);
-          break;
+          if (secondPlayerTurn) {
+            console.log(playerNames[1] + " wins!");
+            gameBoard.removeEventListener("click", (e));
+            break;
+          } else {
+            console.log(playerNames[0] + " wins!");
+            gameBoard.removeEventListener("click", (e));
+            break;
+          }
         }
       }
+    }
+  }
+  const checkWhoseTurn = () => {
+    if (secondPlayerTurn) {
+      secondPlayerTurn = false;
+    } else {
+      secondPlayerTurn = true;
     }
   }
 
@@ -42,5 +57,42 @@ const game = (() => {
     getPlayerName,
     newGame,
     checkForWin,
+    checkWhoseTurn
   };
 })();
+
+const newGameButton = document.getElementById("newGameButton");
+newGameButton.addEventListener("click", (e) => {
+  game.newGame();
+});
+
+const setNameButton = document.getElementById("setNameButton");
+const nameFormContainer = document.getElementById("nameFormContainer");
+setNameButton.addEventListener("click", (e) => {
+  if (nameFormContainer.style.visibility === "visible") {
+    nameFormContainer.style.visibility = "hidden";
+  } else {
+    nameFormContainer.style.visibility = "visible";
+  }
+});
+
+const gameBoard = document.getElementById("gameBoard");
+gameBoard.addEventListener("click", (e) => {
+  if (e.target.classList.contains("boardSquare")) {
+    const squareIndex = parseInt(e.target.getAttribute("data-array-index"));
+    switch (squareIndex) {
+      case 0:
+        console.log("helloworld");
+        break;
+      case 1:
+        console.log(game.getValue(0));
+        break;
+      case 2:
+        console.log(game.getValue(1));
+        break;
+      case 3:
+        console.log(game.getPlayerName(0));
+        break;
+    }
+  }
+});
